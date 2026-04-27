34843772c744cf995cbdf5ff5033cb7cde9d40d3
pritamprasanna <pritamprasanna@gmail.com>
2026-04-27 15:10:17 +0530

    writing-auth-middleware

--- src/app.js
+++ src/app.js
@@ -2,10 +2,27 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res) => {
-  res.send('hello from the server');
+const { adminAuth, userAuth } = require('./middleware/auth');
+
+//Handel Auth Middleware
+app.use('/admin', adminAuth);
+
+app.post('/user/login', (req, res) => {
+  res.send('User logged in succesfully');
 });
 
-app.listen(3000, () => {
-  console.log('server is successfully listening on port 3000...');
+app.get('/user', userAuth, (req, res) => {
+  res.send('User data sent');
+});
+
+app.get('/admin/getAllData', (req, res) => {
+  res.send('User Data Sent');
+});
+
+app.get('/admin/deleteUser', (req, res) => {
+  res.send('Deleted a User');
+});
+
+app.listen(7777, () => {
+  console.log('server is successfully listening on port 7777...');
 });
